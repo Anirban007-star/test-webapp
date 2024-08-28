e2652c6ff83f4060228cd7d401d0fd23e774a271
Anirban007-star <79010999+Anirban007-star@users.noreply.github.com>
2024-08-28 11:53:59 +0530

    removed secret key

--- Test-Webapp/provider.tf
+++ Test-Webapp/provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA47CRZGBL6ZRU6Z45"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 resource "aws_instance" "my-ec2" {
   ami           = "ami-00fa32593b478ad6e"
